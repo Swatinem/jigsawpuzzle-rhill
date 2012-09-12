ce0879ef188a6f36440537a30d2646ad15be79b5
Arpad Borsos <arpad.borsos@googlemail.com>
2012-09-12 12:27:01 +0200

    further modularize

--- lib.js
+++ lib.js
@@ -3,7 +3,19 @@ var libs = {
 	Segment: 'segment',
 	Bbox: 'bbox',
 	Bezier: 'bezier',
-	Profile: 'profile'
+	Profile: 'profile',
+	ProfileRandomizer: 'profilerandomizer',
+	Side: 'side',
+	Contour: 'contour',
+	Polygon: 'polygon',
+	PuzzleTile: 'puzzletile',
+	PuzzleSourceTile: 'puzzlesourcetile',
+	PuzzleTransientTile: 'puzzletransienttile',
+	PuzzleDisplayTile: 'puzzledisplaytile',
+	PuzzlePart: 'puzzlepart',
+	PuzzlePiece: 'puzzlepiece',
+	PuzzlePreview: 'puzzlepreview',
+	PuzzleBed: 'puzzlebed'
 };
 for (var name in libs) {
 	var path = './lib/' + libs[name];
